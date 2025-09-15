4b351970b51992ef31141886dfdca6a40356883e
hamv5d <mirhammadbaloch@gmail.com>
2025-09-16 04:38:50 +0500

    frontend

--- backend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  contact: { type: String, required: true, unique: true }, // email or phone
-  fullName: { type: String, required: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("User", UserSchema);
